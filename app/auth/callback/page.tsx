4482718751e8650b75bb4bbdafe286dda0def622
Prince <techtune.it.solutions@gmail.com>
2025-08-07 19:10:04 -0500

    callback page again

--- app/auth/callback/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/auth/callback/page.tsx
-
-export default function AuthCallbackPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <p>Processing login...</p>
-    </div>
-  )
-}
